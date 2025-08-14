86cb3abae9119ce7709bcc1eb36ec54cf99322eb
Pétur Þór Haraldsson <peturh@islandspostur.is>
2025-08-14 15:41:59 +0000

    syntax

--- modules/ui/dm-lightdm.nix
+++ modules/ui/dm-lightdm.nix
@@ -3,6 +3,6 @@
   services.displayManager = {
     lightdm.enable = true;
     sddm.enable = lib.mkForce false;
-    gdm.enable  = lib.mkForce false;
+    # gdm.enable  = lib.mkForce false; # Removed: not a valid option
   };
 }
